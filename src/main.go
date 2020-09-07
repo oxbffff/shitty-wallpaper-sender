2100062e8bd3b28533c07c7145e6df0f866d1641
oxbfff <off0xbffff@gmail.com>
2020-09-07 19:52:58 +0300

    fixes

--- src/main.go
+++ src/main.go
@@ -1,94 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
-	"math/rand"
-	"net/http"
 	"net/url"
-	"regexp"
+	"os"
 	"strconv"
 	"strings"
-	"time"
-)
-
-const (
-	token = ""
 )
 
 var (
 	updatesCh = make(chan Updates)
-	offset  int
+	token     = os.Getenv("TOKENWBOT")
+	offset    int
 )
-
-func getPhotoURL() (string, error) {
-	resp, err := http.Get(
-		fmt.Sprintf(
-			"https://wall.alphacoders.com/by_category.php?id=3&filter=HD&page=%d",
-			rand.Intn(100),
-		),
-	)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	re := regexp.MustCompile("<img(.*)src=\"(https://images.*)\" alt")
-	links := re.FindAllStringSubmatch(string(body), -1)
-	if links == nil {
-		return "", errors.New("no links found")
-	}
-
-	re = regexp.MustCompile("thumb.*-")
-	return re.ReplaceAllString(links[rand.Intn(len(links)-2)+1][2], ""), nil
-}
-
-func getPrettyJSON(body io.ReadCloser) (string, error) {
-	parsedBody, err := ioutil.ReadAll(body)
-	if err != nil {
-		return "", err
-	}
-
-	var prettyJSON bytes.Buffer
-	err = json.Indent(&prettyJSON, parsedBody, "", "\t")
-	if err != nil {
-		return "", err
-	}
-
-	return string(prettyJSON.Bytes()), nil
-}
-
-func checkIfCommand(entities []MessageEntity) bool {
-	for _, entity := range entities {
-		if entity.Type == "bot_command" {
-			return true
-		}
-	}
-
-	return false
-}
-
-func doRequestToAPI(method string, values *url.Values) (io.ReadCloser, error) {
-	resp, err := http.PostForm(
-		fmt.Sprintf("https://api.telegram.org/bot%s/%s", token, method),
-		*values,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.Body, nil
-}
 
 func sendMessage(chatID int, text string) error {
 	body, err := doRequestToAPI("sendMessage", &url.Values{"chat_id": {strconv.Itoa(chatID)}, "text": {text}})
@@ -120,34 +45,6 @@ func sendPhoto(chatID int, photoURL string) error {
 	return nil
 }
 
-func processingUpdates() {
-	for {
-		newUpdates := <-updatesCh
-
-		for _, update := range newUpdates.Result {
-			if checkIfCommand(update.Message.Entities) {
-				if strings.Contains(update.Message.Text, "/start") {
-					err := sendMessage(update.Message.Chat.ID, "Hello! I can send anime wallpaper")
-					if err != nil {
-						log.Println(err)
-					}
-				} else if strings.Contains(update.Message.Text, "/getpic") {
-					go func() {
-						photoURL, err := getPhotoURL()
-						if err != nil {
-							log.Println(err)
-						}
-						err = sendPhoto(update.Message.Chat.ID, photoURL)
-						if err != nil {
-							log.Println(err)
-						}
-					}()
-				}
-			}
-		}
-	}
-}
-
 func getUpdates() {
 	for {
 		body, err := doRequestToAPI("getUpdates", &url.Values{"offset": {strconv.Itoa(offset)}, "timeout": {"30"}})
@@ -173,9 +70,43 @@ func getUpdates() {
 	}
 }
 
+func processingUpdates() {
+	for {
+		newUpdates := <-updatesCh
+
+		for _, update := range newUpdates.Result {
+			if checkIfCommand(update.Message.Entities) {
+				if strings.Contains(update.Message.Text, "/start") {
+					err := sendMessage(update.Message.Chat.ID, "Hello! I can send anime wallpaper")
+					if err != nil {
+						log.Println(err)
+					}
+				} else if strings.Contains(update.Message.Text, "/pic") {
+					go func() {
+						photoURL, err := getPhotoURL()
+						if err != nil {
+							log.Println(err)
+						}
+						err = sendPhoto(update.Message.Chat.ID, photoURL)
+						if err != nil {
+							log.Println(err)
+						}
+					}()
+				} else if strings.Contains(update.Message.Text, "/sub") {
+					err := sendMessage(update.Message.Chat.ID, "You subscribed to send photos. Use /unsub to undo")
+					if err != nil {
+						log.Println(err)
+					}
+				}
+			}
+		}
+	}
+}
+
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	log.Println("TOKEN: " + token)
 
 	go getUpdates()
 	processingUpdates()
